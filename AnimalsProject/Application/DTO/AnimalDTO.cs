c3252ee189bc646f98ce1de0abebf43b986fc1dd
Yuri Danchyshyn <yurij98.dan@gmail.com>
2020-02-18 20:05:11 +0200

    create dto

---
+++ AnimalsProject/Application/DTO/AnimalDTO.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Common;
+using Domain.Enums;
+using System;
+
+namespace Application.DTO
+{
+    public class AnimalDTO : AnimalBase
+    {
+        public long Id { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public double Weight { get; set; }
+        public double WithersHeight { get; set; }
+        public double NeckCircumference { get; set; }
+        public bool IsAdopted { get; set; }
+        public bool ContinuatitonOfTreatment { get; set; }
+        public long ChipNumber { get; set; }
+        public Gender Gender { get; set; }
+        public Sterialization Sterialization { get; set; }
+        public long AddressId { get; set; }
+        public long CategoryId { get; set; }
+        public long FoodId { get; set; }
+    }
+}
