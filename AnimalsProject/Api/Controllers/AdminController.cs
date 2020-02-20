3163cb2c8d00952ebe27b779244ca52c030d9477
Andrii Kopach <andrii_kopach@epam.com>
2020-02-20 12:50:26 +0200

    add admincontroller for adding email of new admin

---
+++ AnimalsProject/Api/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using Application.DTO;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IMainAdminService _adminService;
+
+        public AdminController(IMainAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNewEmailAsync(AdminEmailDto model)
+        {
+            if (ModelState.IsValid && await _adminService.AddEmailAsync(model))
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpGet]
+        public IEnumerable<IdentityUser> GetUnconfirmedAdmins()
+        {
+            return _adminService.GetUnconfirmedAdmins();
+        }
+    }
+}
