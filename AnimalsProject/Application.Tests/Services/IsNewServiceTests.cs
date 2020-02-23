5513ce5e997ab1cad90f67ac95d867cb95217c8b
“Roman <yakubovychr@gmail.com>
2020-02-24 00:28:16 +0200

    add template of tests

---
+++ AnimalsProject/Application.Tests/Services/IsNewServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace Application.Tests.Services
+{
+    public class IsNewServiceTests
+    {
+        [Test]
+        public void UpdateIsNewCheckbox_SuccessOperation()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public void GetIsNewById_SuccessOperation()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public void DeleteIsNew_SuccessOperation()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ AnimalsProject/Application/Services/IsNewSelectedService.cs
@@ -0,0 +1,46 @@
+﻿using Application.DTO;
+using Application.Interfaces;
+using Domain.Interfaces;
+using Domain.Models;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class IsNewSelectedService : IIsNewSelectedService
+    {
+
+        private readonly IRepository<Animal> _animalRepository;
+
+        public IsNewSelectedService(IRepository<Animal> animalRepository)
+        {
+            _animalRepository = animalRepository;
+        }
+
+        public async Task UpdateIsNewCheckbox(IsNewDto animal)
+        {
+            if (!animal.IsNew)
+            {
+                var animalId = await _animalRepository.GetByIdAsync(animal.Id);
+                animalId.FoundDate = animal.FoundDate;
+                await _animalRepository.Update(animalId);
+                await _animalRepository.SaveAsync();
+            }
+        }
+
+        public async Task<IsNewDto> GetIsNewById(long id)
+        {
+            var animal = await _animalRepository.GetByIdAsync(id);
+            return new IsNewDto()
+            {
+                FoundDate = animal.FoundDate
+            };
+        }
+
+        public async Task DeleteIsNewById(long id)
+        {
+            var animalId = await _animalRepository.GetByIdAsync(id);
+            await _animalRepository.Remove(animalId);
+            await _animalRepository.SaveAsync();
+        }
+    }
+}
